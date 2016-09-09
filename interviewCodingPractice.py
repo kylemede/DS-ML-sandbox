3152a4d45cf9e92a17e1efc8bddf3f88316cec5a
kylemede <kylemede@gmail.com>
2016-09-09 15:50:24 +0900

    added further functions for possible questions.

--- interviewCodingPractice.py
+++ interviewCodingPractice.py
@@ -1,7 +1,17 @@
 import sys
 import ast
 import cmath
+from _ast import Num
  
+def bad_char_finder(ss):
+    """Find the character in the string that is not a number or [ ] , ( )"""
+    print "\n"+"^"*10+" problem with one of the characters provided "+"^"*10+"\n"
+    for s in ss:
+        if s not in ['[',']','(',')',',',' ']:
+            if s.isdigit()==False:
+                print "Character '"+s+"' is not approved for use with this function."
+    print "\n"+"^"*65+"\n"
+
 def cmd_to_verts():
     """ Convert command line arguments into vertices. 
     This function can accept arguments passed in on the command line in the 
@@ -26,22 +36,31 @@ def cmd_to_verts():
         if len(args[1:])==3:
             # inputs were of format '(x1,y1) (x2,y2) (x3,y3)' 
             #                    or '[x1,y1] [x2,y2] [x3,y3]'
-            pt1 = ast.literal_eval(args[1])
-            pt2 = ast.literal_eval(args[2])
-            pt3 = ast.literal_eval(args[3])
+            l=[]
+            for i in range(1,4):
+                try:
+                    l.append(ast.literal_eval(args[i]))
+                except:
+                    bad_char_finder(args[i])
+                    raise IOError()
+            [pt1,pt2,pt3] = l
         else:
             raise IOError("\n\n"+s)
     elif len(args[1:])==1:
         if ("[[" in args[1]) or ("((" in args[1]):
             # The input is '((x1,y1),(x2,y2),(x3,y3))' 
             #           or '[[x1,y1],[x2,y2],[x3,y3]]'
-            [pt1,pt2,pt3] = ast.literal_eval(args[1])  
+            try:
+                [pt1,pt2,pt3] = ast.literal_eval(args[1])  
+            except:
+                bad_char_finder(args[1])
+                raise IOError()
     else:
         raise IOError("\n\n"+s)
-        
+
     # let user know what we got out of their command line arguments
-    print "Argument(s) pass in were:\n"+repr(args[1:])+"\n"
-    print "This was converted to:\n"+repr([pt1,pt2,pt3])  
+    print "\nArgument(s) pass in were:\n"+repr(args[1:])+"\n"
+    print "This was converted to:\n"+repr([pt1,pt2,pt3])+"\n"
     
     return  (pt1,pt2,pt3)
     
@@ -65,15 +84,24 @@ def cmd_to_sides():
     if len(args[1:])>1:
         if len(args[1:])==3:
             # inputs were of format 'a' 'b' 'c'
-            a = ast.literal_eval(args[1])
-            b = ast.literal_eval(args[2])
-            c = ast.literal_eval(args[3])
+            l=[]
+            for i in range(1,4):
+                try:
+                    l.append(ast.literal_eval(args[i]))
+                except:
+                    bad_char_finder(args[i])
+                    raise IOError()
+            [a,b,c] = l
         else:
             raise IOError("\n\n"+s)
     elif len(args[1:])==1:
         if ("[" in args[1]) or ("(" in args[1]):
             # The input is '(a,b,c)' or '[a,b,c]'
-            [a,b,c] = ast.literal_eval(args[1])  
+            try:
+                [a,b,c] = ast.literal_eval(args[1])  
+            except:
+                bad_char_finder(args[1])
+                raise IOError()
     else:
         raise IOError("\n\n"+s)
         
@@ -157,10 +185,10 @@ def quadratic():
     
     print("The solutions are {0}".format(s1) + " and {0}".format(s2)+"\n")
     
-def is_prime():
+def is_prime(num=4):
     """ Determine if the input is a prime number or not."""
     ## prompt user or something to get number
-    num=4 ## HACK$$$$$$$$$
+    ## REMOVE DEFAULT VALUE IN INPUTS
     
     ## Check if it is a prime number
     tf = all(num%i for i in xrange(2,num))
@@ -170,11 +198,59 @@ def is_prime():
         tf_txt = ' is NOT a prime number\n'
     
     print("\n"+str(num)+tf_txt)
+    
+    
+def find_factors(num=136):
+    """ Find the factors of the input number."""
+    ## prompt user or something to get number
+    ## REMOVE DEFAULT VALUE IN INPUTS
+    
+    ## make a list of all the factors
+    factors = [] 
+    for i in range(1,num+1):
+        if num%i==0:
+            factors.append(i)
+            
+    print "The factors of "+str(num)+" are:\n"+repr(factors)
+    
+def primes_in_interval(low=4,high=59):
+    """ Find all the prime numbers in an interval"""
+    ## prompt user or something to get the lower and upper limits of interval
+    ## REMOVE DEFAULT VALUE IN INPUTS
+    
+    ## find all the primes
+    primes = []
+    for num in range(low,high+1):
+        if num>1:
+            if all(num%i for i in xrange(2,num)):
+                primes.append(num)
+    
+    print "Prime numbers in the interval "+repr((low,high))+" are:"
+    print repr(primes)
 
+def armstrong_number(num=371):
+    """ Determine if number is an 'Armstrong Number'"""
+    
+    sum = 0
+    t = num 
+    while t>0:
+        d = t%10
+        t//=10
+        sum+=d**3
+        
+    if sum==num:
+        print(num,"is an Armstrong Number")
+    else:
+        print(num,"is not an Armstrong Number")
+    
+    
+    
+    
 if __name__ == '__main__':
-    triangle_area()
+    #triangle_area()
     #quadratic()
     #is_prime()
+    find_factors()
     
     
     
